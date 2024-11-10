73edc6b54bd07008eeb53f6ea81f1d68d8596496
Vinaya-bhosale <vinayabhosale25@gmail.com>
2024-11-10 09:08:59 -0800

    list data type first program

---
+++ display list.py
@@ -0,0 +1,14 @@
+lis=[1,2,3,4,5,6,7,8,9,10]
+print(lis)
+print(type(lis ) )
+print(len(lis ))
+print(reversed(lis ))
+print(lis[2:4])
+print(lis[6])
+print(lis[0])
+print(lis.pop())
+print(lis.append(11) )
+print(lis)
+print(lis.append(12) )
+print(lis)
+
